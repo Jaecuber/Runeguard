7a2084e097dc173f328f3f2ff46f5bcaa039f486
Jaecuber <pokemon.jaelas@gmail.com>
2026-03-24 15:32:03 -0700

    Collisions and map editing

--- assets/map/MainTileset.tsx
+++ assets/map/MainTileset.tsx
@@ -1,4 +1,144 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="MainTileset" tilewidth="16" tileheight="16" tilecount="4050" columns="50">
+<tileset version="1.10" tiledversion="1.10.2" name="MainTileset" tilewidth="16" tileheight="16" tilecount="4050" columns="50">
  <image source="tileset.png" width="800" height="1296"/>
+ <tile id="1301">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="15.9375" y="4.8125">
+    <polygon points="0,0 -3.375,-1.5 -5.8125,-1.0625 -6.375,1.25 -6.9375,3.3125 -9,3.8125 -9.6875,2.9375 -11.5625,2.8125 -13.625,3.625 -14.25,5.1875 -14.5,9.4375 -14.875,11.1875 -16.625,10.875 -16.25,2.625 -14.4375,0.5625 -10.0625,0.125 -8.8125,0.3125 -8.125,-2 -6.5,-3.8125 -2.625,-4.0625 0.8125,-3"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1302">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.25" y="4.625">
+    <polygon points="0,0 3.125,-2.5 7.375,-2.5625 8.25,-0.9375 10.375,-0.5 11.0625,-1.75 14.4375,-1.5 15.4375,-0.375 16.875,-0.6875 16.5625,-2.8125 14,-4.5625 10.1875,-4.375 9.5,-3.75 8.4375,-5 4.5625,-5.6875 -0.3125,-4.5625 -0.4375,-1.5625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1303">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.375" y="4.25">
+    <polygon points="0,0 -1.0625,-1.6875 -7.125,-1.4375 -8.75,0.8125 -10.375,-1.9375 -16.5,-1.5625 -16.3125,1.0625 -11.0625,1.1875 -8.5625,3.1875 -6.25,0 -2.0625,0.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1304">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.375" y="15.75">
+    <polygon points="0,0 -0.375,-5.875 -3.25,-8.125 -3.3125,-11.8125 -6.25,-14.5625 -12.6875,-15 -16.4375,-10.4375 -16.5625,-7.5 -12,-13.125 -6.75,-12.75 -4.8125,-11.0625 -4.8125,-8 -1.3125,-5 -1.1875,0.3125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1354">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.25" y="0.125">
+    <polygon points="0,0 4.3125,5.6875 6.1875,3.125 3.125,-0.625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1355">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.375" y="16.125">
+    <polygon points="0,0 -0.3125,-6.125 -3.1875,-8.5 -3.5,-11.9375 -6.625,-15.125 -12.5625,-15.125 -16.5,-10.9375 -16.5625,-9.5625 -11.875,-13.4375 -6.875,-13.625 -4.625,-9.75 -3.5,-6.75 -1.4375,-2.1875 -1.6875,-0.3125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1405">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.375" y="0.0625">
+    <polygon points="0,0 -1.5625,3.375 -3.5625,6.8125 -1.125,9.375 -0.25,14.25 -0.5625,16.25 -5.0625,15.5625 -5.8125,-0.5625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1455">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0625" y="0">
+    <polygon points="0,0 0,16.1875 -2.4375,16 -2.875,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1505">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12" y="16.3125">
+    <polygon points="0,0 4.3125,-4.5625 4,-9.4375 1.9375,-11 4.1875,-12.625 4.0625,-16.375 -12,-16.375 -12.0625,-0.4375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1555">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="12.5625" y="-0.125">
+    <polygon points="0,0 1.1875,4.6875 1.5625,9.25 0.0625,12 -3.0625,14.125 -2.4375,15.6875 -12.5625,16.125 -12.625,0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1605">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.125" y="-0.875">
+    <polygon points="0,0 -0.125,1.625 3.0625,5.875 3.1875,11.0625 -1.1875,15.125 -0.6875,16.8125 -11.4375,16.875 -11.9375,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1655">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.4375" y="16">
+    <polygon points="0,0 -0.25,-2.625 3.75,-5.9375 3.75,-11.0625 2.5625,-14.125 1.25,-16.125 1.0625,-17.3125 -11.8125,-17.0625 -11.5,0.3125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1700">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.75" y="0.125">
+    <polygon points="0,0 1.9375,1.9375 -0.9375,5.6875 -0.6875,8.6875 -0.875,9.625 2.375,9.9375 5.125,8.8125 6.5625,6.25 8.375,7.75 10.5,7.8125 11.4375,6.0625 13.25,4.75 14.8125,4.5625 14.6875,-1.5625 -0.1875,-1.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1701">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.1875" y="3.375">
+    <polygon points="0,0 -0.1875,4.375 3.125,4.75 1.5,7.0625 1.6875,12.625 15.8125,12.625 15.8125,-3.25 -0.25,-3.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1704">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11.625" y="16.0625">
+    <polygon points="0,0 0.1875,-4.5 2.9375,-5.25 3.875,-8.6875 4.6875,-10.0625 4.625,-17.3125 -12.375,-17 -12.4375,-0.375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1705">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.1875" y="7.875">
+    <polygon points="0,0 10.5625,-2.125 12.375,-6.6875 12.875,-7.875 12.5625,-10.25 -1.75,-10.25 -1.5625,-0.25"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1751">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2.3125" y="0.25">
+    <polygon points="0,0 1.3125,2.1875 -1.5,5.8125 -0.1875,10.125 4.4375,9.5 6.6875,7.0625 8.1875,7.25 10.9375,8.25 12.3125,5.25 13.8125,5.0625 16.5625,5.0625 16.1875,-2.75 -0.0625,-2.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1752">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.125" y="13.0625">
+    <polygon points="0,0 1.3125,1.125 5.75,0.9375 7.6875,-0.875 10,0.25 13.625,0.0625 15.3125,-1.9375 16.375,-2.25 16.5,-13.375 -0.6875,-13.4375 -0.5625,-0.6875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1753">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.125" y="13.5">
+    <polygon points="0,0 4,0.8125 6.8125,-1.1875 9.375,-1.25 11.5625,0.25 14.125,-1.0625 14.75,-2.5 16.0625,-3.625 17.25,-4.375 16.8125,-15.1875 -1.5625,-14.875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1754">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.5" y="8.1875">
+    <polygon points="0,0 5.5,-1.25 11,-3.0625 12.5,-7.9375 12.1875,-10.625 -2.1875,-10.5 -1.25,0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
--- assets/map/Rocks.tsx
+++ assets/map/Rocks.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Rocks" tilewidth="16" tileheight="16" tilecount="247" columns="13">
+<tileset version="1.10" tiledversion="1.10.2" name="Rocks" tilewidth="16" tileheight="16" tilecount="247" columns="13">
  <image source="Spritesheets/Rocks.png" width="208" height="304"/>
  <tile id="21">
   <objectgroup draworder="index" id="2">
@@ -19,6 +19,20 @@
  <tile id="35">
   <objectgroup draworder="index" id="2">
    <object id="1" x="5.81818" y="5.90909"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="4" x="2.0625" y="0">
+    <polygon points="0,0 16.625,0.0625 16.5,13.75 11,14 3.875,12.9375 0.0625,9.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="13.875" y="0.1875">
+    <polygon points="0,0 -17.5,-0.125 -17.5,13.75 -7.8125,13.6875 -5.6875,10.8125 -2.875,10.75 0.4375,7.6875"/>
+   </object>
   </objectgroup>
  </tile>
 </tileset>
