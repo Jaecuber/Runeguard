eb754aa1282e69c7cd7d3165a461fc7e077fa140
Jaecuber <pokemon.jaelas@gmail.com>
2026-03-25 15:32:46 -0700

    Hitboxes and Starter Area work

--- assets/map/Buildings.tsx
+++ assets/map/Buildings.tsx
@@ -1,4 +1,116 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Buildings" tilewidth="16" tileheight="16" tilecount="5625" columns="75">
+<tileset version="1.10" tiledversion="1.10.2" name="Buildings" tilewidth="16" tileheight="16" tilecount="5625" columns="75">
  <image source="Spritesheets/Buildings.png" width="1200" height="1200"/>
+ <tile id="851">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.3636" y="11.0909">
+    <polygon points="0,0 -10.2727,-0.0909091 -10.3636,5 -0.0909091,5.09091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="852">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.0909" y="11.0909">
+    <polygon points="0,0 -16.0909,0 -16.1818,4.90909 0.0909091,4.90909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="853">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0909" y="10.4545">
+    <polygon points="0,0 -10.0909,-0.272727 -10.0909,5.45455 -0.0909091,5.54545"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="925">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16.2727" y="10.9091">
+    <polygon points="0,0 -10.3636,-0.181818 -10.2727,4.72727 -0.181818,4.90909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="926">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.09091" y="0">
+    <polygon points="0,0 0.181818,10.7273 -6.18182,10.9091 -6.18182,15.9091 3.90909,16.0909 3.81818,-0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="928">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="16" y="11">
+    <polygon points="0,0 -7.09091,-0.272727 -7,-10.9091 -9.90909,-11 -9.90909,4.72727 0,4.90909"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="929">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0909" y="15.9091">
+    <polygon points="0,0 0,-4.90909 -10.1818,-5 -10.1818,0.0909091"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1000">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.09091" y="0.0909091">
+    <polygon points="0,0 -0.0909091,15.9091 3.90909,15.9091 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1004">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.90909" y="0">
+    <polygon points="0,0 4.27273,0.0909091 4.09091,16 0,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1075">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0909" y="0">
+    <polygon points="0,0 -0.0909091,10.9091 6.09091,10.9091 6,16.0909 -4.18182,15.9091 -4.18182,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1076">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10">
+    <polygon points="0,0 10,0.0909091 10,5.90909 0.0909091,6.18182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1078">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.81818" y="15.9091">
+    <polygon points="0,0 0.0909091,-5.27273 10.2727,-5 10.2727,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1079">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0909091" y="10.8182">
+    <polygon points="0,0 6.09091,0 5.90909,-10.7273 9.90909,-10.8182 9.90909,5.09091 -0.181818,5.18182"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1151">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="0">
+    <polygon points="0,0 3.90909,0 3.72727,12.1818 10.0909,12.4545 10.0909,15.9091 -0.181818,16"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1152">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10.3636">
+    <polygon points="0,0 16,0.181818 16,5.72727 0.181818,5.72727"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1153">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6.18182" y="0.0909091">
+    <polygon points="0,0 3.81818,0 3.81818,15.9091 -6.18182,16 -6.09091,11 -0.181818,10.9091"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
