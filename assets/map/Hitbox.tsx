6f9fc03e44f58b1644df0737d1ee5f661d69632c
Jaecuber <pokemon.jaelas@gmail.com>
2026-03-26 15:31:33 -0700

    Hitbox

--- assets/map/Hitbox.tsx
+++ assets/map/Hitbox.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="Hitbox" tilewidth="16" tileheight="16" tilecount="1" columns="1">
  <image source="Spritesheets/Hitbox.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,16 16,16 16,0 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
