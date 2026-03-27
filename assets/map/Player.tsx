fb7e06ae87c3c83b3abbbd7f4ff942ba1c81b939
Jaecuber <pokemon.jaelas@gmail.com>
2026-03-27 15:26:55 -0700

    Map updates

--- assets/map/Player.tsx
+++ assets/map/Player.tsx
@@ -1,11 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Player" tilewidth="32" tileheight="32" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.10.2" name="Player" tilewidth="96" tileheight="80" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <properties>
    <property name="atlasAsset" value="OBJECTS"/>
    <property name="speed" type="float" value="4"/>
   </properties>
-  <image source="Objects/player.png" width="32" height="32"/>
+  <image width="32" height="32" source="Objects/player.png"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="atlasAsset" value="OBJECTS"/>
+   <property name="speed" type="float" value="4"/>
+  </properties>
+  <image width="96" height="80" source="Objects/newPlayer.png"/>
  </tile>
 </tileset>
