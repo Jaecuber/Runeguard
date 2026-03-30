e168ae3e92f059c826589b691152db69570df509
Jaecuber <pokemon.jaelas@gmail.com>
2026-03-29 20:38:12 -0700

    Player Hitbox Accuracy

--- assets/map/Player.tsx
+++ assets/map/Player.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="Player" tilewidth="96" tileheight="80" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.12.0" name="Player" tilewidth="22" tileheight="34" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1" type="GameObject">
+ <tile id="1" x="37" y="24" width="22" height="34" type="GameObject">
   <properties>
    <property name="animation" value="IDLE"/>
    <property name="animationSpeed" type="float" value="1"/>
@@ -13,7 +13,7 @@
    <object id="1" x="47" y="54" width="0.25">
     <ellipse/>
    </object>
-   <object id="2" x="39.6364" y="51.5909" width="17.0909" height="7.45455">
+   <object id="2" x="2.3447" y="27.3409" width="17.0909" height="7.45455">
     <ellipse/>
    </object>
   </objectgroup>
